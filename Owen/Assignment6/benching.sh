eb27b0b0b75f16e028d3dd5d43257b4a4adbed2d
Daniel <danielowen_b@hotmail.com>
2018-02-22 20:45:31 +0100

    init ass6

---
+++ Owen/Assignment6/benching.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+N=5000
+NUMBS=(1 2 4 8 16 32)
+
+REFS=./ref_output_data/ellipse_N_05000_after200steps.gal
+INP=./input_data/ellipse_N_05000.gal
+
+
+
+make clean
+make 
+gcc -std=c11  compare_gal_files/compare_gal_files.c  -o comp -lm
+
+
+for var in ${NUMBS[*]}; do
+    echo "RUNNING galsim: 5000 ${INP} 200 1e-5 0.25 0 ${var}"
+    /usr/bin/time -p ./galsim 5000 ${INP} 200 1e-5 0.25 0 ${var}
+    echo ""
+    echo "RUNNING comp: $5000 ./result.gal ${REFS}"
+    ./comp 5000 ./result.gal ${REFS} > tmp.txt
+    echo ""
+    grep pos_maxdiff tmp.txt | grep 00000 && exit 1
+    grep pos_maxdiff tmp.txt | grep 000 || exit 1
+
+done
+
---
+++ Owen/Assignment6/testing_diff_size.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+N=5000
+NUMBS=(1 2 4 8 16 32)
+
+REFS=(./ref_output_data/ellipse_N_05000_after200steps.gal ./ref_output_data/ellipse_N_00100_after200steps.gal)
+INP=(./input_data/ellipse_N_05000.gal ./input_data/ellipse_N_00100.gal ./input_data/ellipse_N_01000.gal)
+
+make clean
+make 
+
+for var in ${NUMBS[*]}; do
+    echo "RUNNING galsim: 5000 ${INP[0]} 200 1e-5 0.25 0 ${var}"
+    /usr/bin/time -f %e ./galsim 5000 ${INP[0]} 200 1e-5 0.25 0 ${var} >> res.csv
+    echo "RUNNING galsim: 1000 ${INP[2]} 200 1e-5 0.25 0 ${var}"
+    /usr/bin/time -f %e ./galsim 1000 ${INP[2]} 200 1e-5 0.25 0 ${var} >> res.csv
+    echo "RUNNING galsim: 100 ${INP[1]} 200 1e-5 0.25 0 ${var}"
+    /usr/bin/time -f %e ./galsim 100 ${INP[1]} 200 1e-5 0.25 0 ${var} >> res.csv
+    cat res.csv
+    echo ""
+done
+
